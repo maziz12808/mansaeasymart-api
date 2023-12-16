178c6464786a44624764c0bb8cd20c659cf4c913
aziz12808 <maziz12808@gmail.com>
2023-12-16 17:10:21 +0500

    product crud api created

---
+++ schema/product.schema.js
@@ -0,0 +1,44 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose
+
+const productSchema = new Schema({
+    category: { 
+        type: Schema.ObjectId, 
+        ref: "Category" 
+    },
+    brand: { 
+        type: Schema.ObjectId, 
+        ref: "Brand" 
+    },
+    title: {
+        type: String, 
+        required: [true,"title is requied"]
+    },
+    price: {
+        type: Number, 
+        required: [true, "price is required"]
+    },
+    discount: {
+        type: Number, 
+        default: 0
+    },
+    stock: {
+        type: Number, 
+        required: [true,"stock is required"]
+    },
+    image: [String],
+    description: {
+        type: String, 
+        required: [true,"description is required"]
+    },
+    createdAt: {
+        type: Date, 
+        default: Date.now
+    },
+    updatedAt: {
+        type: Date, 
+        default: Date.now
+    }
+})
+
+module.exports = mongoose.model("Product",productSchema)
