b95a2e7a49e32bb01292b92b9756e92698e8c807
aziz12808 <maziz12808@gmail.com>
2023-12-16 14:56:07 +0500

    admin crud api created

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 
-var usersRouter = require('./routes/users');
+var adminRouter = require('./routes/admin.route');
 
 var app = express();
 
@@ -20,7 +20,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 
-app.use('/users', usersRouter);
+app.use('/admin', adminRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
